5e5494295116ce82c9d4d45a9354e564dd0a60f5
Serphyus <serphyus@gmail.com>
2021-03-13 00:51:13 +0100

    fixed import error with setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 from importlib import import_module
 
-version = import_module('source.socket_network._version')
+version = import_module('src.socket_network._version')
 
 setup(
     name='socket_network',
     version=version.__version__,
     author='Serphyus',
     packages=find_packages(),
-    package_dir={'': 'source'},
+    package_dir={'': 'src'},
     install_requires=[
         'msgpack>=1.0.2'
     ]
